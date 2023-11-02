c7157cea7a3f5553b63ed216ea529ed39cce72a1
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-02 09:47:22 +0100

    Added output of CH2 resistor with cor input

--- script.py
+++ script.py
@@ -20,10 +20,12 @@ CH2 = []
 for i in range(1024):
     value = str(i)
     device.query(f"OUT:CH0 {value}")
+
     q1 = device.query(f"MEAS:CH1?")
     CH1.append((int(q1)))
     q2 = device.query(f"MEAS:CH2?")
     CH2.append(int(q2))
 
-print(CH1)
+    print(f"On LED: {i} ({ADC(i)} V)    Over resistor: {q2} ({ADC(int(q2))} V)")
+
 
