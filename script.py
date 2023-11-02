ca7ff20f29fc942b7e5651e3450d653a047d3dbf
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-02 09:40:10 +0100

    Added output lists for CH1 and CH2

--- script.py
+++ script.py
@@ -1,17 +1,29 @@
 import pyvisa
 
 
+def ADC(bit):
+    return bit * (3.3 / 1023)
+def DAC(voltage):
+    return voltage / (3.3 / 1023)
+
 rm = pyvisa.ResourceManager("@py")
 
 ports = rm.list_resources()
-
-print(ports)
 
 device = rm.open_resource(
     "ASRL8::INSTR", read_termination="\r\n", write_termination="\n"
 )
 
+CH1 = []
+CH2 = []
+
 for i in range(1024):
     value = str(i)
     device.query(f"OUT:CH0 {value}")
-    print(value)
+    q1 = device.query(f"MEAS:CH1?")
+    CH1.append((int(q1)))
+    q2 = device.query(f"MEAS:CH2?")
+    CH2.append(int(q2))
+
+print(CH1)
+
