bd2e3531c9cfa4ca38ddac7b9fcc9dc601d97940
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-02 12:39:40 +0100

    Added function for devices list

--- script.py
+++ script.py
@@ -2,41 +2,50 @@ import pyvisa
 import matplotlib.pyplot as plt
 import csv
 
+
 class ArduinoVISADevice:
     def __init__(self, port):
         rm = pyvisa.ResourceManager("@py")
-        self.device = rm.open_resource(port, read_termination="\r\n", write_termination="\n")
-    
+        self.device = rm.open_resource(
+            port, read_termination="\r\n", write_termination="\n")
+
     def get_indentification(self):
         return self.device.query("*IDN?")
-    
+
     def set_output_value(self, value):
         self.device.query(f"OUT:CH0 {value}")
-    
+
     def get_output_value(self):
         return int(self.device.query(f"MEAS:CH0?"))
-    
+
     def get_input_value(self, channel):
         return int(self.device.query(f"MEAS:CH{channel}?"))
-    
+
     def get_input_voltage(self, channel):
         return float(self.get_input_value(channel) * (3.3 / 1023))
 
 
+def list_devices():
+    rm = pyvisa.ResourceManager("@py")
+    return rm.list_resources()
+
+
+print(list_devices())
+
 port = "ASRL8::INSTR"
-device = ArduinoVISADevice(port = port)
+device = ArduinoVISADevice(port=port)
 
 U_LED = []
 I_LED = []
 
 for i in range(1024):
-    device.set_output_value(value = i)
-    
-    q1 = device.get_input_value(channel = 1)
-    q2 = device.get_input_value(channel = 2)
+    device.set_output_value(value=i)
 
-    U1 = device.get_input_voltage(channel = 1)
-    U2 = device.get_input_voltage(channel = 2)
+    q1 = device.get_input_value(channel=1)
+    q2 = device.get_input_value(channel=2)
+
+    U1 = device.get_input_voltage(channel=1)
+    U2 = device.get_input_voltage(channel=2)
 
     R = 220
     I = U2 / R
@@ -46,8 +55,8 @@ for i in range(1024):
     I_LED.append(I)
 
 plt.plot(U_LED, I_LED, '.')
+plt.savefig('U_I_char.png')
 plt.show()
-plt.savefig('U_I_char_001.png')
 
 with open('metingen_001.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
@@ -55,6 +64,4 @@ with open('metingen_001.csv', 'w', newline='') as csvfile:
     for u, i in zip(U_LED, I_LED):
         writer.writerow([u, i])
 
-device.set_output_value(value = 0)
-
-
+device.set_output_value(value=0)
