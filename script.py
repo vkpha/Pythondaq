911f25008f64d117a60b6b0c53df2b47630c88c0
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-02 10:12:45 +0100

    Plot U-I characteristic over LED

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import pyvisa
-
+import matplotlib.pyplot as plt
 
 def ADC(bit):
     return bit * (3.3 / 1023)
+
 def DAC(voltage):
-    return voltage / (3.3 / 1023)
+    return voltage / (3.3 / 1023) 
+
 
 rm = pyvisa.ResourceManager("@py")
 
@@ -17,15 +19,31 @@ device = rm.open_resource(
 CH1 = []
 CH2 = []
 
+U_LED = []
+I_LED = []
+
 for i in range(1024):
     value = str(i)
     device.query(f"OUT:CH0 {value}")
-
+    
     q1 = device.query(f"MEAS:CH1?")
     CH1.append((int(q1)))
     q2 = device.query(f"MEAS:CH2?")
     CH2.append(int(q2))
 
-    print(f"On LED: {i} ({ADC(i)} V)    Over resistor: {q2} ({ADC(int(q2))} V)")
+    U2 = ADC(int(q2))
+    R = 220
+    I = U2 / R
+
+    U_l = ADC(i) - U2
+
+    U_LED.append(U_l)
+    I_LED.append(I)
+    #print(f"On LED: {i} ({ADC(i)} V)    Over resistor: {q2} ({ADC(int(q2))} V)")
+
+plt.plot(U_LED, I_LED, '-')
+plt.show()
+
+device.query(f"OUT:CH0 0")
 
 
