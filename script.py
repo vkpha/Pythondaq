af0db8783adbef6e096657c8c7f47f43ec4f273e
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-02 12:34:55 +0100

    Added Control class for device

--- script.py
+++ script.py
@@ -2,32 +2,42 @@ import pyvisa
 import matplotlib.pyplot as plt
 import csv
 
-def ADC(bit):
-    return bit * (3.3 / 1023)
+class ArduinoVISADevice:
+    def __init__(self, port):
+        rm = pyvisa.ResourceManager("@py")
+        self.device = rm.open_resource(port, read_termination="\r\n", write_termination="\n")
+    
+    def get_indentification(self):
+        return self.device.query("*IDN?")
+    
+    def set_output_value(self, value):
+        self.device.query(f"OUT:CH0 {value}")
+    
+    def get_output_value(self):
+        return int(self.device.query(f"MEAS:CH0?"))
+    
+    def get_input_value(self, channel):
+        return int(self.device.query(f"MEAS:CH{channel}?"))
+    
+    def get_input_voltage(self, channel):
+        return float(self.get_input_value(channel) * (3.3 / 1023))
 
-def DAC(voltage):
-    return voltage / (3.3 / 1023) 
 
-rm = pyvisa.ResourceManager("@py")
-
-ports = rm.list_resources()
-
-device = rm.open_resource(
-    "ASRL8::INSTR", read_termination="\r\n", write_termination="\n"
-)
+port = "ASRL8::INSTR"
+device = ArduinoVISADevice(port = port)
 
 U_LED = []
 I_LED = []
 
 for i in range(1024):
-    value = str(i)
-    device.query(f"OUT:CH0 {value}")
+    device.set_output_value(value = i)
     
-    q1 = device.query(f"MEAS:CH1?")
-    q2 = device.query(f"MEAS:CH2?")
+    q1 = device.get_input_value(channel = 1)
+    q2 = device.get_input_value(channel = 2)
 
-    U1 = ADC(int(q1))
-    U2 = ADC(int(q2))
+    U1 = device.get_input_voltage(channel = 1)
+    U2 = device.get_input_voltage(channel = 2)
+
     R = 220
     I = U2 / R
     U_l = U1 - U2
@@ -36,14 +46,15 @@ for i in range(1024):
     I_LED.append(I)
 
 plt.plot(U_LED, I_LED, '.')
-plt.savefig('U_I_char.png')
+plt.show()
+plt.savefig('U_I_char_001.png')
 
-with open('metingen.csv', 'w', newline='') as csvfile:
+with open('metingen_001.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['U', 'I'])
     for u, i in zip(U_LED, I_LED):
         writer.writerow([u, i])
 
-device.query(f"OUT:CH0 0")
+device.set_output_value(value = 0)
 
 
