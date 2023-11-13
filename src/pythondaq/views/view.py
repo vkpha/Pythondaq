0acc6475d4c60e410451d17f14381baebd8b60e4
vkpha <149399516+vkpha@users.noreply.github.com>
2023-11-13 14:46:35 +0100

    Added main body and view command in toml file

--- src/pythondaq/views/view.py
+++ src/pythondaq/views/view.py
@@ -4,26 +4,30 @@ from pythondaq.models.diode_experiment import DiodeExperiment, list_devices
 import matplotlib.pyplot as plt
 import csv
 
-# Print all the ports that are available and set the port
-print(list_devices())
-port = "ASRL8::INSTR"
+def main():
+    # Print all the ports that are available and set the port
+    print(list_devices())
+    port = "ASRL8::INSTR"
 
-# Call the model class
-experiment = DiodeExperiment(port=port)
+    # Call the model class
+    experiment = DiodeExperiment(port=port)
 
-# Use the scan with error function to conduct an experiment N times
-N = 5
-U_LED, U_err, I_LED, I_err = experiment.scan_with_error(
-    N=5, start=0, stop=1024)
+    # Use the scan with error function to conduct an experiment N times
+    N = 5
+    U_LED, U_err, I_LED, I_err = experiment.scan_with_error(
+        N=5, start=0, stop=1024)
 
-# Plot results
-plt.errorbar(U_LED, I_LED, xerr=U_err, yerr=I_err, marker='.')
-plt.savefig('U_I_char.png')
-plt.show()
+    # Plot results
+    plt.errorbar(U_LED, I_LED, xerr=U_err, yerr=I_err, marker='.')
+    plt.savefig('U_I_char.png')
+    plt.show()
 
-# Export to csv file
-with open('metingen_met_error.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['U_mean', 'I_mean', 'U_err', 'I_err'])
-    for u, i, u_err, i_err in zip(U_LED, I_LED, U_err, I_err):
-        writer.writerow([u, i, u_err, i_err])
+    # Export to csv file
+    with open('metingen_met_error.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['U_mean', 'I_mean', 'U_err', 'I_err'])
+        for u, i, u_err, i_err in zip(U_LED, I_LED, U_err, I_err):
+            writer.writerow([u, i, u_err, i_err])
+
+if __name__ == '__main__':
+    main()
